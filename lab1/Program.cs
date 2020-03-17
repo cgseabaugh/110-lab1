072f365aa28fb375466d5ef3b23645ea39a64486
cgseabaugh <56783671+cgseabaugh@users.noreply.github.com>
2020-03-16 19:04:46 -0700

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("This is a method");
+
+            string firstName = "Gabe";
+            Console.WriteLine("My name is: " + firstName);
+
+            int age = 99;
+            float price = 12.34;
+            decimal money = 123.23m;
+            bool isThisCracy = true;
+
+            if(isThisCrazy)
+            {
+                Console.WriteLine("Yeah it is");
+            }
+            else
+            {
+                Console.WriteLine("No, it is not");
+            }
+
+            FtpStyleUriParser(int i=0; int<10; int++){
+                Console.WriteLine("Iteration: " + i);
+            }
+        }
+    }
+}
